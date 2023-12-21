c9ddcf6c5d5ed35b4284981d472c9243597c6551
mehdizebhi <mehdizebhi@yahoo.com>
2023-12-21 11:28:31 +0330

    Add Alert Componnet

---
+++ src/components/base/AlertComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-if="closable" class="alert alert-dismissible fade show" :class="type" role="alert">
+    {{ text }}
+    <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>
+  <div v-else class="alert" :class="type" role="alert">{{ text }}</div>
+</template>
+
+<script>
+export default {
+  name: "AlertComponent",
+  props: {
+    type: {
+      type: String
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    closable: {
+      type: Boolean
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
